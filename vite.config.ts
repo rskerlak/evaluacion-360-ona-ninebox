7cd4f0effab8d9a1f6fc16d1a6379e279a98d46b
rskerlak <70973731+rskerlak@users.noreply.github.com>
2026-04-22 03:39:44 -0300

    Configurar despliegue en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/evaluacion-360-ona-ninebox/',
   plugins: [react(), tailwindcss()],
 })
